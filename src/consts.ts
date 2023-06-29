07f30edc21f2a3464076cc61c71ab07e1eb93f24
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-06-29 15:46:07 +0700

    works 5

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Equn Jerks | Website';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Equn Jerks';
+export const SITE_DESCRIPTION = 'Uh um!';
 export const HOMEPAGE_URL = 'https://ghettoscum.vercel.app';
